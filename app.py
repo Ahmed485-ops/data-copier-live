df2e13c7ca40eddcc7d7da36760a4bb695738e80
Ahmed_Ouelbani <ahmedamin.ouelbani@gmail.com>
2024-05-23 12:09:59 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import sys
+from config import DB_DETAILS
+
+
+def main():
+    """Program takes at least one argument"""
+    #for arg in sys.argv:
+    #    print(arg)
+
+    env = sys.argv[1]
+    db_details = DB_DETAILS[env]
+    print(db_details)
+
+
+if __name__  == '__main__':
+    main()
